c7a7ae4f2d2ad4ffdd33cd87e3a521638fd99213
Keith Moulton <keithm@xmos.com>
2021-03-24 13:09:09 -0400

    cleanup

--- lib_logging/lib_logging.cmake
+++ lib_logging/lib_logging.cmake
@@ -1,23 +1,23 @@
-set(THIS_LIB LIB_LOGGING)
-if(${USE_${THIS_LIB}})
-    set(${THIS_LIB}_FLAGS "-Os")
+if(${USE_LIB_LOGGING})
+    set(LIB_LOGGING_FLAGS "-Os")
 
-    string(TOLOWER ${THIS_LIB} THIS_PATH)
-    file(GLOB_RECURSE ${THIS_LIB}_XC_SOURCES "${${THIS_LIB}_DIR}/${THIS_PATH}/src/*.xc")
-    file(GLOB_RECURSE ${THIS_LIB}_C_SOURCES "${${THIS_LIB}_DIR}/${THIS_PATH}/src/*.c")
-    file(GLOB_RECURSE ${THIS_LIB}_ASM_SOURCES "${${THIS_LIB}_DIR}/${THIS_PATH}/src/*.S")
+    set(LIB_LOGGING_C_SOURCES)
+        "${LIB_LOGGING_DIR}/${THIS_PATH}/src/debug_printf.c"
+    )
+    set(LIB_LOGGING_XC_SOURCES) "")
+    set(LIB_LOGGING_ASM_SOURCES) "")
 
-    set(${THIS_LIB}_SOURCES
-        ${${THIS_LIB}_XC_SOURCES}
-        ${${THIS_LIB}_C_SOURCES}
-        ${${THIS_LIB}_ASM_SOURCES}
+    set(LIB_LOGGING_SOURCES
+        ${LIB_LOGGING_XC_SOURCES}
+        ${LIB_LOGGING_C_SOURCES}
+        ${LIB_LOGGING_ASM_SOURCES}
     )
 
-    set_source_files_properties(${${THIS_LIB}_SOURCES} PROPERTIES COMPILE_FLAGS ${${THIS_LIB}_FLAGS})
+    set_source_files_properties(${LIB_LOGGING_SOURCES} PROPERTIES COMPILE_FLAGS ${LIB_LOGGING_FLAGS})
 
-    set(${THIS_LIB}_INCLUDES
-        "${${THIS_LIB}_DIR}/${THIS_PATH}/api"
+    set(LIB_LOGGING_INCLUDES
+        "${LIB_LOGGING_DIR}/lib_logging/api"
     )
-    message("${COLOR_GREEN}Adding ${THIS_LIB}...${COLOR_RESET}")
+
+    message(STATUS "Adding LIB_LOGGING")
 endif()
-unset(THIS_LIB)
